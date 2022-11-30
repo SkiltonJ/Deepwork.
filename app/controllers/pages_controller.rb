d131dd37c66c1277924a9bd4a1458a7c41adc10d
Jackson Jaensch <jackson.jaensch14@gmail.com>
2022-11-30 14:57:23 +1030

    second graph

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -12,6 +12,7 @@ class PagesController < ApplicationController
     @topic = Topic.new
     @themes = Theme.all # is this accessing the theme templates of the sessions?
     @last_theme = StudySession.last.theme
+    @number_of_sessions = current_user.study_sessions.group_by_day_of_week(:created_at, format: "%a").count
   end
 
   def quickstart
