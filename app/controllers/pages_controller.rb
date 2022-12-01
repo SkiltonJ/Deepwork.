c41169e95b508349c261ebccab3617b5a2cdd0ca
Jackson Jaensch <jackson.jaensch14@gmail.com>
2022-12-01 14:46:08 +1030

    Fix profile

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -12,14 +12,14 @@ class PagesController < ApplicationController
     @topic = Topic.new
     @themes = Theme.all # is this accessing the theme templates of the sessions?
     @number_of_sessions = current_user.study_sessions.group_by_day_of_week(:created_at, format: "%a").count
-    @week_sessions = current_user.study_sessions.group_by_week(:created_at).count.values[0]
-    @week_minutes = current_user.study_sessions.group_by_week(:created_at).sum(:duration).values[0]
+    @week_sessions = current_user.study_sessions.group_by_week(:created_at).count.values[0] || 0
+    @week_minutes = current_user.study_sessions.group_by_week(:created_at).sum(:duration).values[0] || 0
     @week_minutes_max = (@week_minutes + 20).round(-1)
     @week_sessions_max = (@week_sessions + 5).round(-1)
     if current_user.study_sessions.empty?
       @last_theme = Theme.third
     else
-      @last_theme = current_user.study_sessions.last.theme   # this only works if a study session exists,m otherwise it breaks
+      @last_theme = current_user.study_sessions.last.theme # this only works if a study session exists,m otherwise it breaks
     end
     # @last_theme = Theme.third
     @themes_reverse = @themes.reverse
